f99d5f15509dd5f47aa5e2878fa280289a7faec9
Hbuz <m.basilico10@gmail.com>
2018-10-04 00:52:34 +0200

    Changed set, unable to show data

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
 
-
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
 
 class App extends Component {
-
-  
   render() {
+    // const yeahMan = () => 
+    //   client.query({
+    //     query: gql`
+    //       {
+    //         getAllNationalities {
+    //           nationality
+    //           count
+    //         }
+    //       }
+    //     `
+    //   })
+    // }).then( result => console.log(JSON.stringify(result)))
+
+    async function yeahMan(){ 
+      return await client.query({
+        query: gql`
+          {
+            getAllNationalities {
+              nationality
+              count
+            }
+          }
+        `
+      })
+    }
+
+
+    const result = yeahMan();
+
+    console.log("RESS: " + JSON.stringify(result));
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <div>
+          {/* {result.data.getAllNationalities.map(nat => 
+          <h6>{nat}</h6>
+          )} */}
+        </div>
+      </ApolloProvider>
     );
   }
 }
