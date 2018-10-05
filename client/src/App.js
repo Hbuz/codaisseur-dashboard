6eb644596d04383444b03089a8f5454ed69694c7
Hbuz <m.basilico10@gmail.com>
2018-10-05 15:43:49 +0200

    added grid to main

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,11 @@ import { ApolloProvider } from "react-apollo";
 import ApolloClient from "apollo-boost";
 import { Route, Redirect } from 'react-router-dom'
 import FunnyContainer from './components/FunnyContainer'
-import TopBar  from "./components/layout/TopBar"
+import TopBar from "./components/layout/TopBar"
 import NationalitiesContainer from './components/NationalitiesContainer'
 import AverageSalary from './components/AverageSalary'
 import MaleFemaleAge from './components/MaleFemaleAge'
+import Grid from '@material-ui/core/Grid'
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql"
@@ -24,10 +25,36 @@ class App extends Component {
             <TopBar class='header page__header' />
           </nav>
           <main style={{ marginTop: 75 }}>
-            <Route exact path="/" component={FunnyContainer} />
-            <NationalitiesContainer />
-            <AverageSalary />
-            <MaleFemaleAge />
+            <Grid container direction="row" justify="center" alignItems="flex-start">
+              <Grid item>
+                <Grid container>
+                  <Grid item>
+                    <FunnyContainer />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Grid container>
+                  <Grid item>
+                    <NationalitiesContainer />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Grid container>
+                  <Grid item>
+                    <AverageSalary />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Grid container>
+                  <Grid item>
+                    <MaleFemaleAge />
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
           </main>
           <footer>
             {/* <Footer /> */}
--- client/src/components/FunnyContainer.jsx
+++ client/src/components/FunnyContainer.jsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 import Funny from './Funny'
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
-import './layout/Funny.css';
 
 class FunnyContainer extends React.PureComponent {
 
