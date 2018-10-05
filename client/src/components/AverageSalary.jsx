ed14ce4c45d50863ccb45bcd9b9be28124360d4c
michaeledander1 <michaeledander1@gmail.com>
2018-10-05 14:51:10 +0200

    basic avg salary component added

---
+++ client/src/components/AverageSalary.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+export default function AverageSalary() {
+  return (
+  <div className="averageSalary">
+    <div className="btn_averageSalary-size">
+      <p>average 1st salary</p>
+    </div>
+    <div className="btn_averageSalary-number">
+      <p>€2300</p>
+    </div>
+  </div>
+  )
+}
