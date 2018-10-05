e9b48e913469fcd447ae95048f05cd614e31e662
Attila <aranyi8921@gmail.com>
2018-10-05 15:10:31 +0200

    footer test

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,17 @@
 import React, { Component } from "react";
-import "./App.css";
+import "./styles/App.css";
 import { ApolloProvider } from "react-apollo";
 import ApolloClient from "apollo-boost";
 import gql from "graphql-tag";
-
+import Footer from './components/layout/Footer'
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql"
 });
 
 class App extends Component {
   render() {
-    // const yeahMan = () => 
-    //   client.query({
-    //     query: gql`
-    //       {
-    //         getAllNationalities {
-    //           nationality
-    //           count
-    //         }
-    //       }
-    //     `
-    //   })
-    // }).then( result => console.log(JSON.stringify(result)))
-
-    async function yeahMan(){ 
-      return await client.query({
+    const yeahMan = () => 
+      client.query({
         query: gql`
           {
             getAllNationalities {
@@ -33,20 +20,22 @@ class App extends Component {
             }
           }
         `
-      })
-    }
+      }).then( result => result = JSON.stringify(result))
+    
+      const result = yeahMan();
 
-
-    const result = yeahMan();
-
-    console.log("RESS: " + JSON.stringify(result));
+      console.log("RESS: " + JSON.stringify(result))
 
     return (
       <ApolloProvider client={client}>
         <div>
-          {/* {result.data.getAllNationalities.map(nat => 
+          {/* {result.getAllNationalities.map(nat => 
           <h6>{nat}</h6>
           )} */}
+        <nav></nav>
+        <div>
+          <Footer  />
+        </div>
         </div>
       </ApolloProvider>
     );
---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+
+
+const footerStyle = {
+  backgroundColor: "purple",
+  fontSize: "20px",
+  color: "white",
+  borderTop: "1px solid #E7E7E7",
+  textAlign: "center",
+  padding: "20px",
+  position: "fixed",
+  left: "0",
+  bottom: "0",
+  height: "60px",
+  width: "100%"
+};
+
+const phantomStyle = {
+  display: "block",
+  padding: "20px",
+  height: "60px",
+  width: "100%"
+};
+
+function Footer({ children }) {
+  return (
+    <div>
+      <div style={phantomStyle} />
+      <div style={footerStyle}>{children}</div>
+    </div>
+  );
+}
+
+class Hello extends Component() {
+  render() {
+  return (
+    <div>
+      <div>Top Content!</div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div>Sandwich: Keep Scrolling!</div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div>You can scroll and see me!</div>
+    <div>
+    <Hello />
+    <Footer>
+      <span>footer content</span>
+    </Footer>
+  </div>
+  </div>
+  )}}
+export default Hello 
